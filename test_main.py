028b1600c318af753b9582fda0971242da46782c
jhCOR <jhjung9759@naver.com>
2023-07-14 14:32:38 +0900

    (filemodule, multiprocess)

---
+++ test_main.py
@@ -0,0 +1,24 @@
+import time
+import unittest 
+from toolkit import multiprocessor, saveToFile
+
+def check(num):
+    time.sleep(0.3)
+    print(num, end=" ")
+    return num
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_multiprocessor(self):
+        num = 4
+        num_list = [i for i in range(num)]
+        result = multiprocessor(check, num_list, core = 4, return_value=True)
+        self.assertEqual(len(result), num)
+
+    def test_saveToFile(self):
+        num_list = [i for i in range(30)]
+        result = saveToFile(num_list, "test.csv", prefix="./", time_stamp=True)
+        self.assertEqual(result, True)
+
+if __name__ == '__main__':
+    unittest.main()
