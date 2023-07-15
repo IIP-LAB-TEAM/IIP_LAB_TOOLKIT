bfb511dfc68b1be142f9016a0a7cdb73dfdcbd31
jhCOR <jhjung9759@naver.com>
2023-07-15 11:41:36 +0900

    FIX: fix bug in Loss class

--- toolkit/calculate/Loss.py
+++ toolkit/calculate/Loss.py
@@ -11,8 +11,8 @@ class Loss():
         self.device = device
 
     def update(self, pred, label):
-        self.pred = pred if self.pred is None else torch.cat([self.pred, pred])
-        self.label = label if self.label is None else torch.cat([self.label, label])
+        self.pred = self.pred
+        self.label = self.label
 
     def compute(self):
         return self.loss_fun(self.pred.to(self.device), self.label.to(self.device))
